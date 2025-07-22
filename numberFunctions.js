9e33ee8e87b4e6a42613763fe01f283fac9c99c2
Hannah <hannah20022024@gmail.com>
2025-07-22 03:10:38 +0000

    Hannah commit

---
+++ numberFunctions.js
@@ -0,0 +1,3 @@
+import  isPrime from 'prime-number-check'; 
+export const checkNumber = (value) => { return  isPrime(value) ? "The number is prime" : "The number is not prime"; };
+console.log(checkNumber(5));
